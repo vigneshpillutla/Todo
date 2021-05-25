baa46dc4ea09b7f2d2def73d6994a0dfd290606b
Vignesh <vigneshpillutla@gmail.com>
2021-05-25 16:28:37 +0530

    modified images

--- src/components/ToggleTheme.jsx
+++ src/components/ToggleTheme.jsx
@@ -4,8 +4,11 @@ import {useTheme,useThemeUpdate} from './ThemeProvider'
 const ToggleTheme = (props)=>{
     const currentTheme = useTheme();
     const toggleTheme = useThemeUpdate()
+    const toggleClick = ()=>{
+        toggleTheme();
+    }
     return (
-        <div style={{background:currentTheme.toggleButtonBg}} onClick={toggleTheme} id="toggle-outer">
+        <div style={{background:currentTheme.toggleButtonBg}} onClick={toggleClick} id="toggle-outer">
             <div style={{left:currentTheme.toggleIconPosition}} id="toggle-inner">
                 <img src={currentTheme.toggleIcon} alt="" />
             </div>
